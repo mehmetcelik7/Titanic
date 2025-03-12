5f311acc9c7899b7aea1a211fd1021d396d7295d
mehmet <mehmet.cel.ik@hotmail.com>
2025-03-12 20:04:34 +0300

    Done

--- Titanic/Views/MainView.swift
+++ Titanic/Views/MainView.swift
@@ -4,8 +4,8 @@
 //
 //  Created by mehmet Çelik on 12.03.2025.
 //
-
 import SwiftUI
+import CoreML
 
 struct MainView: View {
     @State private var tm: TitanicModel = .init(
@@ -22,28 +22,27 @@ struct MainView: View {
     @State private var showAlert = false
     @State private var survivalRate: Double = -1
     
-    
     var body: some View {
         NavigationStack {
             ZStack {
                 Form {
-                    
+                    // Passenger Class
                     SegmentSectionView(
                         selected: $tm.passengerClass,
                         options: TitanicModel.passengerClasses,
                         sectionTitle: "Passenger Class",
-                        prompt: "What passenger class are you ?"
+                        prompt: "What passenger class are you?"
                     )
                     
-                  
+                    // Gender/sex
                     SegmentSectionView(
                         selected: $tm.sex,
                         options: TitanicModel.genders,
                         sectionTitle: "Gender",
-                        prompt: "What is your gender"
+                        prompt: "What is your gender?"
                     )
-                  
-                    
+
+                    // Age
                     SliderSectionView(
                         value: $tm.age,
                         sectionTitle: "Age",
@@ -53,65 +52,109 @@ struct MainView: View {
                         step: 0.5
                     )
                     
-                    
+                    // Siblings and spouses
                     SliderSectionView(
                         value: $tm.siblingsSpouses,
                         sectionTitle: "Siblings and Spouses",
-                        prompt: "Number of siblings: \(tm.siblingsSpouses.formatted())",
+                        prompt: "Number of siblings/spouses: \(tm.siblingsSpouses.formatted())",
                         min: 0,
                         max: 10,
                         step: 1
                     )
-                   
+
+                    
+                    // Parents and children
                     SliderSectionView(
                         value: $tm.parentsChildren,
                         sectionTitle: "Parents and Children",
-                        prompt: "Number of parents and children \(tm.parentsChildren.formatted())",
+                        prompt: "Numer of parents and children \(tm.parentsChildren.formatted())",
                         min: 0,
                         max: 10,
                         step: 1
                     )
                     
+                    // Ticket price? (in 1910 pounds)
                     SliderSectionView(
                         value: $tm.fare,
-                        sectionTitle: "Ticket price",
-                        prompt: "Ticket and price $\(tm.fare.formatted())",
+                        sectionTitle: "Ticket price? (in 1910 pounds)",
+                        prompt: "Ticket price £\(tm.fare.formatted())",
                         min: 0,
                         max: 600,
-                        step: 1
+                        step: 0.1
                     )
+
+                    // Port
                     SegmentSectionView(
                         selected: $tm.port,
                         options: TitanicModel.ports,
                         sectionTitle: "Port",
                         prompt: "What port did you embark from?"
                     )
-                    
-                    
-                    
+
                 }
+                .scrollIndicators(.hidden)
+                .blur(radius: showAlert ? 5 : 0)
+                .disabled(showAlert)
                 
                 if showAlert {
-                    Text("TODO: Sow result about survival")
+                    Button(action: {
+                        withAnimation {
+                            showAlert.toggle()
+                        }
+                    }, label: {
+                        if let survival {
+                            VStack {
+                                Text(survival ? "SURVIVED!" : "DID NOT SURVIVE")
+
+                                Text("Probability of Survival: \(survivalRate)")
+                            }
+                            .padding()
+                            .background(Color.black)
+                            .foregroundStyle(.white)
+                        }
+                    })
                 }
             }
             .navigationTitle("Surviving the Titanic")
             .toolbar {
                 ToolbarItem(placement: .bottomBar) {
-                    Button(action: {
-                        //TODO:
-                        
-                    }) {
+                    Button(action: compute, label: {
                         Text("Compute")
                             .bold()
-                            .foregroundStyle(.red)
+                            .foregroundStyle(Color.red)
                             .opacity(showAlert ? 0 : 1)
-                    }
+                    })
                 }
             }
         }
+    }
+    func compute() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try TitanicRegressionModel_1(configuration: config)
+            let prediction = try model
+                .prediction(
+                    Pclass: tm.Pclass,
+                    Sex: tm.sex.lowercased(),
+                    Age: tm.age,
+                    SibSp: Int64(tm.siblingsSpouses),
+                    Parch: Int64(tm.parentsChildren),
+                    Fare: tm.fare,
+                    Embarked: String(tm.port.first ?? "C")
+                )
+            
+            // Result between 0 and 1 = Probability of survival
+            survivalRate = prediction.Survived
+            
+            survival = prediction.Survived > 0.5
+            
+        } catch {
+            survival = nil
+        }
+        showAlert = true
         
     }
+    
 }
 
 #Preview {
